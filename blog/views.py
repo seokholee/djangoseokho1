aad359d9365783b94c68e06ee0f387ac30448181
seokholee <ssda0739@gmail.com>
2016-06-08 15:55:14 +0900

    blog.category_detail 뷰 구현

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Category
 
 def index(request):
     category_list = Category.objects.all()
     return render(request, 'blog/index.html', {
      'category_list' : category_list
+    })
+
+def category_detail(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    return render(request, 'blog/category_detail.html', {
+        'category' : category
     })
