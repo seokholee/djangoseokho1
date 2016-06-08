8139b82e31c4a2053041fd0dd0e849ab871372d1
seokholee <ssda0739@gmail.com>
2016-06-08 15:20:14 +0900

    blog.category_list 뷰에서 Category 목록 노출

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Category
 
 def index(request):
-    return render(request, 'blog/index.html')
+    category_list = Category.objects.all()
+    return render(request, 'blog/index.html', {
+     'category_list' : category_list
+    })
