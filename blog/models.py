4a29be6a5eb028b760095f4459a95c17d8731882
seokholee <ssda0739@gmail.com>
2016-06-08 15:11:43 +0900

    blog.Category 모델을 admin에 등록

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,7 @@ class Category(models.Model):
     title = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+def __str__(self):
+    return self.title
+    
