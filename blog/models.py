b144a13365ae817b5e89578a0ce5103124e32d5c
seokholee <ssda0739@gmail.com>
2016-06-08 17:15:44 +0900

    blog.Shop 모델/마이그레이션 적용

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -8,4 +9,14 @@ class Category(models.Model):
 
 def __str__(self):
     return self.title
-    
+
+class Shop(models.Model):
+    category = models.ForeignKey(Category)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    title = models.CharField(max_length=100)
+    number = models.CharField(max_length=100)
+    address = models.TextField()
+    content = models.TextField()
+    photo = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
